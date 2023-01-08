3f915e8c0300a5c272d4516d88d15124416d19bb
Nicholas Fasching <91689117+njfdev@users.noreply.github.com>
2023-01-07 22:13:02 -0500

    Initial Commit

---
+++ Advanced Network Viewer/Advanced_Network_ViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Advanced_Network_ViewerApp.swift
+//  Advanced Network Viewer
+//
+//  Created by Nicholas Fasching on 1/7/23.
+//
+
+import SwiftUI
+
+@main
+struct Advanced_Network_ViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
