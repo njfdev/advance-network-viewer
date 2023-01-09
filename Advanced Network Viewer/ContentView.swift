e970ddbb615995f891d3e34b906fe88634ef8f24
Nicholas Fasching <91689117+njfdev@users.noreply.github.com>
2023-01-08 19:35:05 -0500

    feat: print network adapters to console

--- Advanced Network Viewer/ContentView.swift
+++ Advanced Network Viewer/ContentView.swift
@@ -6,8 +6,36 @@
 //
 
 import SwiftUI
+import Network
 
 struct ContentView: View {
+    init() {
+        let monitor = NWPathMonitor()
+
+        monitor.pathUpdateHandler = { path in
+            if path.status == .satisfied {
+                print("The device is connected to the network.")
+                
+                // Get the list of available interfaces
+                let interfaces = path.availableInterfaces
+                for interface in interfaces {
+                    // Print the interface type (e.g. "Wi-Fi", "Ethernet", etc.)
+                    print(interface.type)
+                    
+                    // Print the interface name (e.g. "en0", "en1", etc.)
+                    print(interface.name)
+                }
+            }
+            else {
+                print("The device is not connected to the network.")
+            }
+        }
+
+        let queue = DispatchQueue(label: "Monitor")
+        monitor.start(queue: queue)
+
+    }
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
